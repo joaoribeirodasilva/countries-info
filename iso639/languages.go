c24716880f76099ef5c0fc3768995dda6328aa0a
joao <joao@git.quimicosfacil.net>
2023-11-24 21:53:33 -0300

    languages.go

--- iso639/languages.go
+++ iso639/languages.go
@@ -2,8 +2,10 @@
 
 package iso639
 
+import "strings"
+
 type Code int
-type Names string
+type Name string
 
 const (
 	CODE_None Code = iota
@@ -220,7 +222,7 @@ const (
 	CODE_Zulu
 )
 
-var codes = map[string]Names{
+var codes = map[Name]Code{
 	"":                CODE_None,
 	"Abkhazian":       CODE_Abkhazian,
 	"Afar":            CODE_Afar,
@@ -293,227 +295,227 @@ var codes = map[string]Names{
 	"Indonesian":      CODE_Indonesian,
 }
 
-var names = map[Code]string{
-	CODE_None                Names = "None",
-	CODE_Abkhazian           Names = "Abkhazian",
-	CODE_Afar                Names = "Afar",
-	CODE_Afrikaans           Names = "Afrikaans",
-	CODE_Akan                Names = "Akan",
-	CODE_Albanian            Names = "Albanian",
-	CODE_Amharic             Names = "Amharic",
-	CODE_Arabic              Names = "Arabic",
-	CODE_Aragonese           Names = "Aragonese",
-	CODE_Armenian            Names = "Armenian",
-	CODE_Assamese            Names = "Assamese",
-	CODE_Avaric              Names = "Avaric",
-	CODE_Avestan             Names = "Avestan",
-	CODE_Aymara              Names = "Aymara",
-	CODE_Azerbaijani         Names = "Azerbaijani",
-	CODE_Bambara             Names = "Bambara",
-	CODE_Bashkir             Names = "Bashkir",
-	CODE_Basque              Names = "Basque",
-	CODE_Belarusian          Names = "Belarusian",
-	CODE_Bengali             Names = "Bengali",
-	CODE_Bislama             Names = "Bislama",
-	CODE_Bosnian             Names = "Bosnian",
-	CODE_Breton              Names = "Breton",
-	CODE_Bulgarian           Names = "Bulgarian",
-	CODE_Burmese             Names = "Burmese",
-	CODE_Catalan             Names = "Catalan",
-	CODE_Valencian           Names = "Catalan",
-	CODE_Chamorro            Names = "Chamorro",
-	CODE_Chechen             Names = "Chechen",
-	CODE_Chichewa            Names = "Chichewa",
-	CODE_Chewa               Names = "Chichewa",
-	CODE_Nyanja              Names = "Chichewa",
-	CODE_Chinese             Names = "Chinese",
-	CODE_Church_Slavonic     Names = "Church Slavonic",
-	CODE_Old_Slavonic        Names = "Old Slavonic",
-	CODE_Old_Church_Slavonic Names = "Old Church Slavonic",
-	CODE_Chuvash             Names = "Chuvash",
-	CODE_Cornish             Names = "Cornish",
-	CODE_Corsican            Names = "Corsican",
-	CODE_Cree                Names = "Cree"
-	CODE_Croatian            Names = "Croatian"
-	CODE_Czech               Names = "Czech"
-	CODE_Danish              Names = "Danish"
-	CODE_Divehi              Names = "Divehi"
-	CODE_Dhivehi             Names = "Dhivehi"
-	CODE_Maldivian           Names = "Maldivian"
-	CODE_Dutch               Names = "Dutch"
-	CODE_Flemish             Names = "Flemish"
-	CODE_Dzongkha            Names = "Dzongkha"
-	CODE_English             Names = "English"
-	CODE_Esperanto           Names = "Esperanto"
-	CODE_Estonian            Names = "Estonian"
-	CODE_Ewe                 Names = "Ewe"
-	CODE_Faroese             Names = "Faroese"
-	CODE_Fijian              Names = "Fijian"
-	CODE_Finnish             Names = "Finnish"
-	CODE_French              Names = "French"
-	CODE_Fulah               Names = "Fulah"
-	CODE_Gaelic              Names = "Gaelic"
-	CODE_Scottish_Gaelic     Names = "Scottish Gaelic"
-	CODE_Galician            Names = "Galician"
-	CODE_Ganda               Names = "Ganda"
-	CODE_Georgian            Names = "Georgian"
-	CODE_German              Names = "German"
-	CODE_Greek               Names = "Greek"
-	CODE_Kalaallisut         Names = "Kalaallisut"
-	CODE_Greenlandic         Names = "Greenlandic"
-	CODE_Guarani             Names = "Guarani"
-	CODE_Gujarati            Names = "Gujarati"
-	CODE_Haitian             Names = "Haitian"
-	CODE_Haitian_Creole      Names = "Haitian"
-	CODE_Hausa               Names = "Hausa"
-	CODE_Hebrew              Names = "Hebrew"
-	CODE_Herero              Names = "Herero"
-	CODE_Hindi               Names = "Hindi"
-	CODE_Hiri_Motu           Names = "Hiri Motu"
-	CODE_Hungarian           Names = "Hungarian"
-	CODE_Icelandic           Names = "Icelandic"
-	CODE_Ido                 Names = "Ido"
-	CODE_Igbo                Names = "Igbo"
-	CODE_Indonesian          Names = "Indonesian"
-	CODE_Interlingua         Names = "Interlingua"
-	CODE_Interlingue         Names = "Interlingue"
-	CODE_Occidental          Names = "Interlingue"
-	CODE_Inuktitut           Names = "Inuktitut"
-	CODE_Inupiaq             Names = "Inupiaq"
-	CODE_Irish               Names = "Irish"
-	CODE_Italian             Names = "Italian"
-	CODE_Japanese            Names = "Japanese"
-	CODE_Javanese            Names = "Javanese"
-	CODE_Kannada             Names = "Kannada"
-	CODE_Kanuri              Names = "Kanuri"
-	CODE_Kashmiri            Names = "Kashmiri"
-	CODE_Kazakh              Names = "Kazakh"
-	CODE_Central_Khmer       Names = "Central Khmer"
-	CODE_Kikuyu              Names = "Kikuyu"
-	CODE_Gikuyu              Names = "Gikuyu"
-	CODE_Kinyarwanda         Names = "Kinyarwanda"
-	CODE_Kirghiz             Names = "Kirghiz"
-	CODE_Kyrgyz              Names = "Kirghiz"
-	CODE_Komi                Names = "Komi"
-	CODE_Kongo               Names = "Kongo"
-	CODE_Korean              Names = "Korean"
-	CODE_Kuanyama            Names = "Kuanyama"
-	CODE_Kwanyama            Names = "Kuanyama"
-	CODE_Kurdish             Names = "Kurdish"
-	CODE_Lao                 Names = "Lao"
-	CODE_Latin               Names = "Latin"
-	CODE_Latvian             Names = "Latvian"
-	CODE_Limburgan           Names = "Limburgan"
-	CODE_Limburger           Names = "Limburgan"
-	CODE_Limburgish          Names = "Limburgan"
-	CODE_Lingala             Names = "Lingala"
-	CODE_Lithuanian          Names = "Lithuanian"
-	CODE_Luba_Katanga        Names = "Luba-Katanga"
-	CODE_Luxembourgish       Names = "Luxembourgish"
-	CODE_Letzeburgesch       Names = "Luxembourgish"
-	CODE_Macedonian          Names = "Macedonian"
-	CODE_Malagasy            Names = "Malagasy"
-	CODE_Malay               Names = "Malay"
-	CODE_Malayalam           Names = "Malayalam"
-	CODE_Maltese             Names = "Maltese"
-	CODE_Manx                Names = "Manx"
-	CODE_Maori               Names = "Maori"
-	CODE_Marathi             Names = "Marathi"
-	CODE_Marshallese         Names = "Marshallese"
-	CODE_Mongolian           Names = "Mongolian"
-	CODE_Nauru               Names = "Nauru"
-	CODE_Navajo              Names = "Navajo"
-	CODE_Navaho              Names = "Navaho"
-	CODE_North_Ndebele       Names = "North Ndebele"
-	CODE_South_Ndebele       Names = "South Ndebele"
-	CODE_Ndonga              Names = "Ndonga"
-	CODE_Nepali              Names = "Nepali"
-	CODE_Norwegian           Names = "Norwegian"
-	CODE_Norwegian_Bokmal    Names = "Norwegian Bokmål"
-	CODE_Norwegian_Nynorsk   Names = "Norwegian Nynorsk"
-	CODE_Sichuan_Yi          Names = "Sichuan Yi"
-	CODE_Nuosu               Names = "Nuosu"
-	CODE_Occitan             Names = "Occitan"
-	CODE_Ojibwa              Names = "Ojibwa"
-	CODE_Oriya               Names = "Oriya"
-	CODE_Oromo               Names = "Oromo"
-	CODE_Ossetian            Names = "Ossetian"
-	CODE_Ossetic             Names = "Ossetic"
-	CODE_Pali                Names = "Pali"
-	CODE_Pashto              Names = "Pashto"
-	CODE_Pushto              Names = "Pashto"
-	CODE_Persian             Names = "Persian"
-	CODE_Polish              Names = "Polish"
-	CODE_Portuguese          Names = "Portuguese"
-	CODE_Punjabi             Names = "Punjabi"
-	CODE_Panjabi             Names = "Punjabi"
-	CODE_Quechua             Names = "Quechua"
-	CODE_Romanian            Names = "Romanian"
-	CODE_Moldavian           Names = "Romanian"
-	CODE_Moldovan            Names = "Romanian"
-	CODE_Romansh             Names = "Romansh"
-	CODE_Rundi               Names = "Rundi"
-	CODE_Russian             Names = "Russian"
-	CODE_Northern_Sami       Names = "Northern Sami"
-	CODE_Samoan              Names = "Samoan"
-	CODE_Sango               Names = "Sango"
-	CODE_Sanskrit            Names = "Sanskrit"
-	CODE_Sardinian           Names = "Sardinian"
-	CODE_Serbian             Names = "Serbian"
-	CODE_Shona               Names = "Shona"
-	CODE_Sindhi              Names = "Sindhi"
-	CODE_Sinhala             Names = "Sinhala"
-	CODE_Sinhalese           Names = "Sinhalese"
-	CODE_Slovak              Names = "Slovak"
-	CODE_Slovenian           Names = "Slovenian"
-	CODE_Somali              Names = "Somali"
-	CODE_Southern_Sotho      Names = "Southern Sotho"
-	CODE_Spanish             Names = "Spanish"
-	CODE_Castilian           Names = "Spanish"
-	CODE_Sundanese           Names = "Sundanese"
-	CODE_Swahili             Names = "Swahili"
-	CODE_Swati               Names = "Swati"
-	CODE_Swedish             Names = "Swedish"
-	CODE_Tagalog             Names = "Tagalog"
-	CODE_Tahitian            Names = "Tahitian"
-	CODE_Tajik               Names = "Tajik"
-	CODE_Tamil               Names = "Tamil"
-	CODE_Tatar               Names = "Tatar"
-	CODE_Telugu              Names = "Telugu"
-	CODE_Thai                Names = "Thai"
-	CODE_Tibetan             Names = "Tibetan"
-	CODE_Tigrinya            Names = "Tigrinya"
-	CODE_Tonga               Names = "Tonga"
-	CODE_Tsonga              Names = "Tsonga"
-	CODE_Tswana              Names = "Tswana"
-	CODE_Turkish             Names = "Turkish"
-	CODE_Turkmen             Names = "Turkmen"
-	CODE_Twi                 Names = "Twi"
-	CODE_Uighur              Names = "Uighur"
-	CODE_Uyghur              Names = "Uighur"
-	CODE_Ukrainian           Names = "Ukrainian"
-	CODE_Urdu                Names = "Urdu"
-	CODE_Uzbek               Names = "Uzbek"
-	CODE_Venda               Names = "Venda"
-	CODE_Vietnamese          Names = "Vietnamese"
-	CODE_Volapuk             Names = "Volapük"
-	CODE_Walloon             Names = "Walloon"
-	CODE_Welsh               Names = "Welsh"
-	CODE_Wolof               Names = "Wolof"
-	CODE_Xhosa               Names = "Xhosa"
-	CODE_Yiddish             Names = "Yiddish"
-	CODE_Yoruba              Names = "Yoruba"
-	CODE_Zhuang              Names = "Zhuang"
-	CODE_Chuang              Names = "Zhuang"
-	CODE_Zulu                Names = "Zulu"	
-} 
+var names = map[Code]Name{
+	CODE_None:                "None",
+	CODE_Abkhazian:           "Abkhazian",
+	CODE_Afar:                "Afar",
+	CODE_Afrikaans:           "Afrikaans",
+	CODE_Akan:                "Akan",
+	CODE_Albanian:            "Albanian",
+	CODE_Amharic:             "Amharic",
+	CODE_Arabic:              "Arabic",
+	CODE_Aragonese:           "Aragonese",
+	CODE_Armenian:            "Armenian",
+	CODE_Assamese:            "Assamese",
+	CODE_Avaric:              "Avaric",
+	CODE_Avestan:             "Avestan",
+	CODE_Aymara:              "Aymara",
+	CODE_Azerbaijani:         "Azerbaijani",
+	CODE_Bambara:             "Bambara",
+	CODE_Bashkir:             "Bashkir",
+	CODE_Basque:              "Basque",
+	CODE_Belarusian:          "Belarusian",
+	CODE_Bengali:             "Bengali",
+	CODE_Bislama:             "Bislama",
+	CODE_Bosnian:             "Bosnian",
+	CODE_Breton:              "Breton",
+	CODE_Bulgarian:           "Bulgarian",
+	CODE_Burmese:             "Burmese",
+	CODE_Catalan:             "Catalan",
+	CODE_Valencian:           "Catalan",
+	CODE_Chamorro:            "Chamorro",
+	CODE_Chechen:             "Chechen",
+	CODE_Chichewa:            "Chichewa",
+	CODE_Chewa:               "Chichewa",
+	CODE_Nyanja:              "Chichewa",
+	CODE_Chinese:             "Chinese",
+	CODE_Church_Slavonic:     "Church Slavonic",
+	CODE_Old_Slavonic:        "Old Slavonic",
+	CODE_Old_Church_Slavonic: "Old Church Slavonic",
+	CODE_Chuvash:             "Chuvash",
+	CODE_Cornish:             "Cornish",
+	CODE_Corsican:            "Corsican",
+	CODE_Cree:                "Cree",
+	CODE_Croatian:            "Croatian",
+	CODE_Czech:               "Czech",
+	CODE_Danish:              "Danish",
+	CODE_Divehi:              "Divehi",
+	CODE_Dhivehi:             "Dhivehi",
+	CODE_Maldivian:           "Maldivian",
+	CODE_Dutch:               "Dutch",
+	CODE_Flemish:             "Flemish",
+	CODE_Dzongkha:            "Dzongkha",
+	CODE_English:             "English",
+	CODE_Esperanto:           "Esperanto",
+	CODE_Estonian:            "Estonian",
+	CODE_Ewe:                 "Ewe",
+	CODE_Faroese:             "Faroese",
+	CODE_Fijian:              "Fijian",
+	CODE_Finnish:             "Finnish",
+	CODE_French:              "French",
+	CODE_Fulah:               "Fulah",
+	CODE_Gaelic:              "Gaelic",
+	CODE_Scottish_Gaelic:     "Scottish Gaelic",
+	CODE_Galician:            "Galician",
+	CODE_Ganda:               "Ganda",
+	CODE_Georgian:            "Georgian",
+	CODE_German:              "German",
+	CODE_Greek:               "Greek",
+	CODE_Kalaallisut:         "Kalaallisut",
+	CODE_Greenlandic:         "Greenlandic",
+	CODE_Guarani:             "Guarani",
+	CODE_Gujarati:            "Gujarati",
+	CODE_Haitian:             "Haitian",
+	CODE_Haitian_Creole:      "Haitian",
+	CODE_Hausa:               "Hausa",
+	CODE_Hebrew:              "Hebrew",
+	CODE_Herero:              "Herero",
+	CODE_Hindi:               "Hindi",
+	CODE_Hiri_Motu:           "Hiri Motu",
+	CODE_Hungarian:           "Hungarian",
+	CODE_Icelandic:           "Icelandic",
+	CODE_Ido:                 "Ido",
+	CODE_Igbo:                "Igbo",
+	CODE_Indonesian:          "Indonesian",
+	CODE_Interlingua:         "Interlingua",
+	CODE_Interlingue:         "Interlingue",
+	CODE_Occidental:          "Interlingue",
+	CODE_Inuktitut:           "Inuktitut",
+	CODE_Inupiaq:             "Inupiaq",
+	CODE_Irish:               "Irish",
+	CODE_Italian:             "Italian",
+	CODE_Japanese:            "Japanese",
+	CODE_Javanese:            "Javanese",
+	CODE_Kannada:             "Kannada",
+	CODE_Kanuri:              "Kanuri",
+	CODE_Kashmiri:            "Kashmiri",
+	CODE_Kazakh:              "Kazakh",
+	CODE_Central_Khmer:       "Central Khmer",
+	CODE_Kikuyu:              "Kikuyu",
+	CODE_Gikuyu:              "Gikuyu",
+	CODE_Kinyarwanda:         "Kinyarwanda",
+	CODE_Kirghiz:             "Kirghiz",
+	CODE_Kyrgyz:              "Kirghiz",
+	CODE_Komi:                "Komi",
+	CODE_Kongo:               "Kongo",
+	CODE_Korean:              "Korean",
+	CODE_Kuanyama:            "Kuanyama",
+	CODE_Kwanyama:            "Kuanyama",
+	CODE_Kurdish:             "Kurdish",
+	CODE_Lao:                 "Lao",
+	CODE_Latin:               "Latin",
+	CODE_Latvian:             "Latvian",
+	CODE_Limburgan:           "Limburgan",
+	CODE_Limburger:           "Limburgan",
+	CODE_Limburgish:          "Limburgan",
+	CODE_Lingala:             "Lingala",
+	CODE_Lithuanian:          "Lithuanian",
+	CODE_Luba_Katanga:        "Luba-Katanga",
+	CODE_Luxembourgish:       "Luxembourgish",
+	CODE_Letzeburgesch:       "Luxembourgish",
+	CODE_Macedonian:          "Macedonian",
+	CODE_Malagasy:            "Malagasy",
+	CODE_Malay:               "Malay",
+	CODE_Malayalam:           "Malayalam",
+	CODE_Maltese:             "Maltese",
+	CODE_Manx:                "Manx",
+	CODE_Maori:               "Maori",
+	CODE_Marathi:             "Marathi",
+	CODE_Marshallese:         "Marshallese",
+	CODE_Mongolian:           "Mongolian",
+	CODE_Nauru:               "Nauru",
+	CODE_Navajo:              "Navajo",
+	CODE_Navaho:              "Navaho",
+	CODE_North_Ndebele:       "North Ndebele",
+	CODE_South_Ndebele:       "South Ndebele",
+	CODE_Ndonga:              "Ndonga",
+	CODE_Nepali:              "Nepali",
+	CODE_Norwegian:           "Norwegian",
+	CODE_Norwegian_Bokmal:    "Norwegian Bokmål",
+	CODE_Norwegian_Nynorsk:   "Norwegian Nynorsk",
+	CODE_Sichuan_Yi:          "Sichuan Yi",
+	CODE_Nuosu:               "Nuosu",
+	CODE_Occitan:             "Occitan",
+	CODE_Ojibwa:              "Ojibwa",
+	CODE_Oriya:               "Oriya",
+	CODE_Oromo:               "Oromo",
+	CODE_Ossetian:            "Ossetian",
+	CODE_Ossetic:             "Ossetic",
+	CODE_Pali:                "Pali",
+	CODE_Pashto:              "Pashto",
+	CODE_Pushto:              "Pashto",
+	CODE_Persian:             "Persian",
+	CODE_Polish:              "Polish",
+	CODE_Portuguese:          "Portuguese",
+	CODE_Punjabi:             "Punjabi",
+	CODE_Panjabi:             "Punjabi",
+	CODE_Quechua:             "Quechua",
+	CODE_Romanian:            "Romanian",
+	CODE_Moldavian:           "Romanian",
+	CODE_Moldovan:            "Romanian",
+	CODE_Romansh:             "Romansh",
+	CODE_Rundi:               "Rundi",
+	CODE_Russian:             "Russian",
+	CODE_Northern_Sami:       "Northern Sami",
+	CODE_Samoan:              "Samoan",
+	CODE_Sango:               "Sango",
+	CODE_Sanskrit:            "Sanskrit",
+	CODE_Sardinian:           "Sardinian",
+	CODE_Serbian:             "Serbian",
+	CODE_Shona:               "Shona",
+	CODE_Sindhi:              "Sindhi",
+	CODE_Sinhala:             "Sinhala",
+	CODE_Sinhalese:           "Sinhalese",
+	CODE_Slovak:              "Slovak",
+	CODE_Slovenian:           "Slovenian",
+	CODE_Somali:              "Somali",
+	CODE_Southern_Sotho:      "Southern Sotho",
+	CODE_Spanish:             "Spanish",
+	CODE_Castilian:           "Spanish",
+	CODE_Sundanese:           "Sundanese",
+	CODE_Swahili:             "Swahili",
+	CODE_Swati:               "Swati",
+	CODE_Swedish:             "Swedish",
+	CODE_Tagalog:             "Tagalog",
+	CODE_Tahitian:            "Tahitian",
+	CODE_Tajik:               "Tajik",
+	CODE_Tamil:               "Tamil",
+	CODE_Tatar:               "Tatar",
+	CODE_Telugu:              "Telugu",
+	CODE_Thai:                "Thai",
+	CODE_Tibetan:             "Tibetan",
+	CODE_Tigrinya:            "Tigrinya",
+	CODE_Tonga:               "Tonga",
+	CODE_Tsonga:              "Tsonga",
+	CODE_Tswana:              "Tswana",
+	CODE_Turkish:             "Turkish",
+	CODE_Turkmen:             "Turkmen",
+	CODE_Twi:                 "Twi",
+	CODE_Uighur:              "Uighur",
+	CODE_Uyghur:              "Uighur",
+	CODE_Ukrainian:           "Ukrainian",
+	CODE_Urdu:                "Urdu",
+	CODE_Uzbek:               "Uzbek",
+	CODE_Venda:               "Venda",
+	CODE_Vietnamese:          "Vietnamese",
+	CODE_Volapuk:             "Volapük",
+	CODE_Walloon:             "Walloon",
+	CODE_Welsh:               "Welsh",
+	CODE_Wolof:               "Wolof",
+	CODE_Xhosa:               "Xhosa",
+	CODE_Yiddish:             "Yiddish",
+	CODE_Yoruba:              "Yoruba",
+	CODE_Zhuang:              "Zhuang",
+	CODE_Chuang:              "Zhuang",
+	CODE_Zulu:                "Zulu",
+}
 
 func GetCode(name string) Code {
 
-	return codes[strings.ToLower(name)]
+	return codes[Name(strings.ToLower(name))]
 }
 
-func GetName(code Code) Names {
+func GetName(code Code) Name {
 
-	return names[Code]
+	return names[code]
 }
